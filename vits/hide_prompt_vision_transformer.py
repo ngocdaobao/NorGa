2358437e8f1c9285f194a7db52f8d818928a49d3
ngocdaobao <dbaongoc2312@gmail.com>
2025-08-20 23:51:53 +0700

    BBB

--- vits/hide_prompt_vision_transformer.py
+++ vits/hide_prompt_vision_transformer.py
@@ -967,6 +967,7 @@ def vit_base_patch16_224(pretrained=False, **kwargs):
     """ ViT-Base (ViT-B/16) from original paper (https://arxiv.org/abs/2010.11929).
     ImageNet-1k weights fine-tuned from in21k @ 224x224, source https://github.com/google-research/vision_transformer.
     """
+    print('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB')
     model_kwargs = dict(patch_size=16, embed_dim=768, depth=12, num_heads=12, **kwargs)
     model = _create_vision_transformer('vit_base_patch16_224', pretrained=pretrained, **model_kwargs)
     return model
